f6a1d435ca0a48b5f110076d4884aeab781a3f22
Tyler Hansen <Tyler Hansen@TYLER>
2016-11-16 19:08:01 -0800

    Add project files.

---
+++ FrozenCritters/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrozenCritters.Startup))]
+namespace FrozenCritters
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
