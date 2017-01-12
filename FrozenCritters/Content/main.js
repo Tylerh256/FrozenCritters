b08722bd4a108369ed42f442575bafb2846c19c2
Tyler Hansen <Tylerh256@yahoo.com>
2017-01-12 13:27:01 -0800

    Mobile improvements

--- FrozenCritters/Content/main.js
+++ FrozenCritters/Content/main.js
@@ -5,3 +5,12 @@ $('.navPhone').on('click', function (e) {
     // If menu is already showing, slide it up. Otherwise, slide it down.
     $('.ulnavbar').slideToggle();
 });
+
+$(window).resize(function () {
+    if ($('.ulnavbar').is(":hidden") && $(window).width() >= 983) {
+        $('.ulnavbar').show();
+    }
+    else if ($(window).width() < 982) {
+        $('.ulnavbar').hide();
+    }
+});
