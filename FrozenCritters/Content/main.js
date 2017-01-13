fdcb3e353d9d8646602ec9fad1754b2bf04a3bce
Tyler Hansen <Tylerh256@yahoo.com>
2017-01-13 15:50:26 -0800

    Working on IE/Edge issue

--- FrozenCritters/Content/main.js
+++ FrozenCritters/Content/main.js
@@ -1,4 +1,22 @@
-﻿
+﻿function msieversion() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+    }
+    else  // If another browser, return 0
+    {
+        alert('otherbrowser');
+    }
+
+    return false;
+}
+
+msieversion();
+
 $('.navPhone').on('click', function (e) {
     // Prevent link from jumping to the top of the page
     e.preventDefault();
@@ -7,6 +25,7 @@ $('.navPhone').on('click', function (e) {
 });
 
 $(window).resize(function () {
+
     if ($('.ulnavbar').is(":hidden") && $(window).innerWidth() > 983) {
         $('.ulnavbar').removeAttr('style');
         $('.ulnavbar').show();
