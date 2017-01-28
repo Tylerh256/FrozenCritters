1cbd802449370eb06b58fec03ac1ea7974fd231f
Tyler Hansen <Tylerh256@yahoo.com>
2017-01-27 16:01:32 -0800

    Edge works properly now. Internet Explorer still needs to be fixed

--- FrozenCritters/Content/main.js
+++ FrozenCritters/Content/main.js
@@ -1,23 +1,4 @@
-﻿function msieversion() {
-
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf("MSIE ");
-
-    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-    {
-        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
-    }
-    else  // If another browser, return 0
-    {
-        alert('otherbrowser');
-    }
-
-    return false;
-}
-
-msieversion();
-
-$('.navPhone').on('click', function (e) {
+﻿$('.navPhone').on('click', function (e) {
     // Prevent link from jumping to the top of the page
     e.preventDefault();
     // If menu is already showing, slide it up. Otherwise, slide it down.
