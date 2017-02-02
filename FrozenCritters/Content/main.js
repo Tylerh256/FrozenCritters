c29b9750bbd1edb99ee3ac2934448fc36f2d54cb
Tyler Hansen <Tylerh256@yahoo.com>
2017-02-01 16:22:18 -0800

    made more tweeks to navbar

--- FrozenCritters/Content/main.js
+++ FrozenCritters/Content/main.js
@@ -2,17 +2,14 @@
     // Prevent link from jumping to the top of the page
     e.preventDefault();
     // If menu is already showing, slide it up. Otherwise, slide it down.
-    $('.ulnavbar').slideToggle();
+    $('.ulnavbar').toggle();
 });
 
 $(window).resize(function () {
 
-    if ($(".ulnavbar").is(":hidden") && $(window).innerWidth() > 983) {
+    if ($(".ulnavbar").is(":hidden") && $(window).innerWidth() > 999) {
         $(".ulnavbar").removeAttr("style");
         $('.ulnavbar').show();
-    }
-    else if($(window).innerWidth() > 1000){
-        $(".ulnavbar").css("display", "initial");
     }
     else if ($(window).width() <= 983) {
         $(".ulnavbar").hide();
